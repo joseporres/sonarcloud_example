3c8bbcc17693fc58ab3a197aa2e1919c37ab968e
joseporres <jose.porres@utec.edu.pe>
2022-08-09 12:43:43 -0500

    example 5

--- example/hello_test.go
+++ example/hello_test.go
@@ -3,7 +3,7 @@ package example_test
 import (
 	"testing"
 
-	"github.com/ototadana/golang-sonarcloud-example/example"
+	"github.com/joseporres/sonarcloud_example/example"
 	"github.com/stretchr/testify/assert"
 )
 
